7ef54b8324ca7f1336bd0baf5ace583209d51da6
marcell <fourmarz@yahoo.com>
2021-05-03 23:06:43 -0400

    testing done

--- test_flask_route.py
+++ test_flask_route.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from app import app
-from models import db, User
+from models import db, User, Post
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///users_test'
 app.config['SQLALCHEMY_ECHO'] = False
@@ -19,6 +19,7 @@ class UserViewsTest(TestCase):
     def setUp(self):
         """add a user to database"""
 
+        Post.query.delete()
         User.query.delete()
 
         user = User(first_name="gary", last_name='the snail')
@@ -26,6 +27,14 @@ class UserViewsTest(TestCase):
         db.session.commit()
 
         self.user_id = user.id
+
+        post = Post(title='test', content='this is a test', created_at='1-1-21', user_id=self.user_id)
+        db.session.add(post)
+        db.session.commit()
+
+        self.post = post
+        self.post_id = post.id
+
     
     def tearDown(self):
         """ clean up after test """
@@ -67,4 +76,52 @@ class UserViewsTest(TestCase):
             html = res.get_data(as_text=True)
 
             self.assertEqual(res.status_code, 200)
+
+    def test_users_post_new(self):
+        with app.test_client() as client:
+            res = client.get(f'/users/{self.user_id}/post/new')
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h1>Add Post for gary the snail</h1>', html)
+
+    def test_users_post_method(self):
+        with app.test_client() as client:
+            test_data = {"title": self.post.title, "content": self.post.content, "created_at": self.post.content, "user_id": self.user_id}
+            res = client.post(f'/users/{self.user_id}/posts/new', data=test_data, follow_redirects=True)
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn(f'<li> <a href="/posts/{self.post_id}">test</a> </li>', html)
+
+    def test_post_post_id(self):
+        with app.test_client() as client:
+            res = client.get(f'/posts/{self.post.id}')
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn(f'<h1>{self.post.title}</h1>', html)
+
+    def test_post_post_id_edit(self):
+        with app.test_client() as client:
+            res = client.get(f'/posts/{self.post.id}/edit')
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h1>Edit Post</h1>', html)
+
+    def test_post_post_id_edit_post_req(self):
+        with app.test_client() as client:
+            test_data = {"title": "test", "content": "this is a test"}
+            res = client.post(f'/posts/{self.post.id}/edit', data=test_data, follow_redirects=True)
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('<h1>test</h1>', html)
+            self.assertIn('<p>this is a test</p>', html)
+            
+
+    
+
+
 
